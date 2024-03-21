fd399df6d43f7a2953bf1409ed1cce0d02904989
besankarroum <besan.karroum@stu.ocsb.ca>
2024-03-21 13:39:52 -0400

    updated quiz
    changed quiz

--- quiz.js
+++ quiz.js
@@ -1,37 +1,34 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
-<>
 
-let num = prompt("Enter a tempurature in fahrenheight");{
+let num = prompt("Enter a tempurature in fahrenheight");
 
-console.log("That tempurature is " + ((num-32)(*5/9)) + "in celcius");
-}
-
-    //Program 2: Grade Calculator
-    let temp = 80;
-
-let grade  = prompt(grade <= 100%)
-
-(90% <=grade<= 100%){
-      console.log('90% and above')
-}
-if(80% <=grade<= 89%){
-      console.log('80% - 89%')
-}
-else if(70% <=grade<= 79%){
-      console.log('70% - 79%')
-}
-else if(60% <=grade<= 69%){
-      console.log('60% - 69%')
-}
-else{
-    console.log('below 60%')
-}
-    //Program 3: Leap Year Checker
+console.log("That tempurature is " + ((num-32)*(5/9)) + "in celcius");
 
 
-    //Program 4: Largest Number Finder
-let num1 = number prompt ()
+  //Program 2: Grade Calculator
+ //let grade = 80;
 
-    </></>
+ //let grade  = prompt(grade <= 100%)
+  
+//  (90% <=grade<= 100%){
+ //       console.log('90% and above')
+ // }
+  //if(80% <=grade<= 89%){
+ //       console.log('80% - 89%')
+ // }
+ // else if(70% <=grade<= 79%){
+ //       console.log('70% - 79%')
+ // }
+ // else if(60% <=grade<= 69%){
+ //       console.log('60% - 69%')
+//  }
+//  else{
+ //     console.log('below 60%')
+ // }
+      //Program 3: Leap Year Checker
+  
+  
+      //Program 4: Largest Number Finder
+    
