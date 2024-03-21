68d498947e06a047922cde9d92df48d7c53030ad
besankarroum <besan.karroum@stu.ocsb.ca>
2024-03-21 13:27:39 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,9 +1,35 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
+<>
 
-//Program 2: Grade Calculator
+let num = prompt("Enter a tempurature in fahrenheight");{
 
-//Program 3: Leap Year Checker
+console.log("That tempurature is " + ((num-32)(*5/9)) + "in celcius");
+}
 
-//Program 4: Largest Number Finder
+    //Program 2: Grade Calculator
+    let temp = 80;
+
+let grade  = prompt(grade <= 100%)
+
+(90% <=grade<= 100%){
+      console.log('90% and above')
+}
+if(80% <=grade<= 89%){
+      console.log('80% - 89%')
+}
+else if(70% <=grade<= 79%){
+      console.log('70% - 79%')
+}
+else if(60% <=grade<= 69%){
+      console.log('60% - 69%')
+}
+else{
+    console.log('below 60%')
+}
+    //Program 3: Leap Year Checker
+
+
+    //Program 4: Largest Number Finder
+    </></>
